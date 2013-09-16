3fdfa7abd04e82d393bf09a56c9fab612907d821
Ivan Brazza <dudeman1996@gmail.com>
2013-09-16 12:25:22 +0100

    Update Parsley.js container

--- submit-a-testimonial/index.php
+++ submit-a-testimonial/index.php
@@ -40,15 +40,31 @@
   <div class="form">
     <h1>Submit A Testimonial</h1>
     <form action="index.php" method="POST" data-validate="parsley">
-      <label for="name">Name</label><br />
-      <input type="text" name="name" id="name" data-required="true" data-trigger="change" data-error="Please enter your name"><br />
-      <label for="email">Email</label><br />
-      <input type="text" name="email" id="email" data-type="email" data-trigger="change" data-required="true" data-error="Please enter your email"><br />
-      <label for="location">Location</label><br />
-      <input type="text" name="location" id="location"><br />
-      <label for="testimonial">Testimonial</label><br />
-      <textarea name="testimonial" id="testimonial" rows="6" cols="40" data-trigger="change" data-required="true" data-error="Please enter your testimonial"></textarea><br /><br />
-      <input type="submit" value="Submit Testimonial">
+      <div>
+        <label for="name">Name</label>
+        <div class="parsley-container">
+          <input type="text" name="name" id="name" data-required="true" data-trigger="change" data-error="Please enter your name">
+        </div>
+      </div>
+      <div>
+        <label for="email">Email</label>
+        <div class="parsley-container">
+          <input type="text" name="email" id="email" data-type="email" data-trigger="change" data-required="true" data-error="Please enter your email">
+        </div>
+      </div>
+      <div>
+        <label for="location">Location</label>
+        <div class="parsley-container">
+          <input type="text" name="location" id="location">
+        </div>
+      </div>
+      <div>
+        <label for="testimonial">Testimonial</label>
+        <div class="parsley-container">
+          <textarea name="testimonial" id="testimonial" rows="6" cols="40" data-trigger="change" data-required="true" data-error="Please enter your testimonial"></textarea>
+        </div>
+      </div>
+      <input type="submit" id="submit-testimonial" value="Submit Testimonial">
     </form>
   </div>
 <?php include("../footer.php"); ?>
