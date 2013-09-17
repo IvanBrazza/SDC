3b2bdd0cda4e46a802f26d94b046e51e6887db4d
Ivan Brazza <dudeman1996@gmail.com>
2013-09-17 09:20:31 +0100

    Add reCAPTCHA to submit testimonial page

--- submit-a-testimonial/index.php
+++ submit-a-testimonial/index.php
@@ -2,37 +2,62 @@
   require("../common.php");
   $title = "Submit A Testimonial";
   
+  if(!empty($_GET['e']))
+  {
+    if ($_GET['e'] === "captcha")
+    {
+      $display_message = "Incorrect captcha.";
+    }
+  }
+
   if (!empty($_POST))
   {
-    $query = "
-      INSERT INTO testimonials (
-        name,
-        email,
-        location,
-        testimonial
-      ) VALUES (
-        :name,
-        :email,
-        :location,
-        :testimonial
-      )
-    ";
+    // Check reCAPTCHA
+    require_once('../recaptchalib.php');
+    $privatekey = "6LePfucSAAAAAHkrfHOrSYPPvJqf6rCiNnhWT77L";
+    $resp = recaptcha_check_answer($privatekey, $_SERVER["REMOTE_ADDR"],$_POST["recaptcha_challenge_field"], $_POST["recaptcha_response_field"]);
 
-    $query_params = array(
-      ':name' => $_POST['name'],
-      ':email' => $_POST['email'],
-      ':location' => $_POST['location'],
-      ':testimonial' => $_POST['testimonial']
-    );
-
-    try
+    if (!$resp->is_valid) 
     {
-      $stmt = $db->prepare($query);
-      $result = $stmt->execute($query_params);
+      // What happens when the CAPTCHA was entered incorrectly
+      header("Location: ../submit-a-testimonial/?e=captcha");
+      die();
     }
-    catch(PDOException $ex)
+    else
     {
-      die("Failed to execute query: " . $ex->getMessage());
+      $query = "
+        INSERT INTO testimonials (
+          name,
+          email,
+          location,
+          testimonial
+        ) VALUES (
+          :name,
+          :email,
+          :location,
+          :testimonial
+        )
+      ";
+
+      $query_params = array(
+        ':name' => $_POST['name'],
+        ':email' => $_POST['email'],
+        ':location' => $_POST['location'],
+        ':testimonial' => $_POST['testimonial']
+      );
+  
+      try
+      {
+        $stmt = $db->prepare($query);
+        $result = $stmt->execute($query_params);
+      }
+      catch(PDOException $ex)
+      { 
+        die("Failed to execute query: " . $ex->getMessage());
+      }
+
+      header("Location: ../testimonials/");
+      die();
     }
   }
 ?>
@@ -64,6 +89,16 @@
           <textarea name="testimonial" id="testimonial" rows="6" cols="40" data-trigger="change" data-required="true" data-error="Please enter your testimonial"></textarea>
         </div>
       </div>
+      <div class="error">
+        <span class="error_message">
+          <?php echo $display_message; ?>
+        </span>
+      </div>
+      <?php
+        require_once("../recaptchalib.php");
+        $publickey = "6LePfucSAAAAAKlUO3GQKgfXCd7SvIhtFjBH5F9Z";
+        echo recaptcha_get_html($publickey, $error = null, $use_ssl = false);
+      ?>
       <input type="submit" id="submit-testimonial" value="Submit Testimonial">
     </form>
   </div>
