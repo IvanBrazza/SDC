9a1ed0b385bbc672ba4f6ea5de327b1f567c19de
Ivan Brazza <dudeman1996@gmail.com>
2013-09-19 12:13:53 +0100

    Remove testmail.php

--- testmail.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $to = "dudeman1996@gmail.com";
-  $subject = "test";
-  $message = "test";
-  $from = "info@ivanbrazza.biz";
-  $headers = "From:" . $from;
-  mail($to,$subject,$message,$headers);
-  echo("Mail sent.");
-?>
