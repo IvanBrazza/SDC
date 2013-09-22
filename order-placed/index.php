f5124726dcadeb6c5f83c1f659588ccb7e5e9d29
Ivan Brazza <dudeman1996@gmail.com>
2013-09-22 15:35:58 +0100

    Require common.php for order-placed

--- order-placed/index.php
+++ order-placed/index.php
@@ -1,4 +1,8 @@
-<?php $title = "Thanks for your order!"; include("../lib/header.php"); ?>
+<?php
+  require("../lib/common.php");
+  $title = "Thanks for your order!";
+?>
+<?php include("../lib/header.php"); ?>
   <div class="container">
     <h3>Thank you for your order, a copy of it has been emailed to you. Any further updates to your order will be sent to you by email</h3>
   </div>
