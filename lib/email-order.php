a6505872c37a6ae2d4dc1af4500cc8735afccdb2
Ivan Brazza <dudeman1996@gmail.com>
2013-09-23 22:10:09 +0100

    Send emails through namecheap's open-xchange
    Hotmail doesn't accept emails from dynamic IP's as most
    are spam. Namecheap's Open-Xchange email services should
    be more reliable too.

--- lib/email-order.php
+++ lib/email-order.php
@@ -2,11 +2,30 @@
   /**
     email-order.php - email a copy of the order to the customer.
   **/
+  require_once "Mail.php";
+
   $from       = 'Star Dream Cakes <admin@ivanbrazza.biz>';
   $to         = $_SESSION['user']['email'];
   $subject    = 'Your Order With Star Dream Cakes';
-  $message    = "Hi " . $_SESSION['user']['first_name'] . ",\n\nHere is the order you've requested: \n\n\n" . $_POST['order'] . "\n\n\nIt should be with you by " . $_POST['datetime'] . ".\n\nIf you have any problems don't hesitate to call.\n\nThanks,\nFran";
-  $headers    = "From:" . $from;
+  $body       = "Hi " . $_SESSION['user']['first_name'] . ",\n\nHere is the order you've requested: \n\n\n" . $_POST['order'] . "\n\n\nIt should be with you by " . $_POST['datetime'] . ".\n\nIf you have any problems don't hesitate to call.\n\nThanks,\nFran";
+  $headers    = array(
+                  'From'    => $from,
+                  'To'      => $to,
+                  'Subject' => $subject
+                );
 
-  mail($to, $subject, $message, $headers);
+  $smtp = Mail::factory('smtp', array(
+    'host'      => 'oxmail.registrar-servers.com',
+    'port'      => '25',
+    'auth'      => true,
+    'username'  => 'admin@ivanbrazza.biz',
+    'password'  => 'inspiron1520',
+    'timeout'   => '30'
+  ));
+
+  $mail = $smtp->send($to, $headers, $body);
+
+  if (PEAR::isError($mail)) {
+    die("Failed to send email: " . $mail->getMessage());
+  }
 ?>
--- lib/email-verification.php
+++ lib/email-verification.php
@@ -3,11 +3,30 @@
     email-verification.php - send an email verification to the customer
     when they register.
   **/
+  require_once "Mail.php";
+
   $from     = "Star Dream Cakes <admin@ivanbrazza.biz>";
   $to       = $_POST['email'];
   $subject  = "Register Your Email";
-  $message  = "Hi " . $_POST['first_name'] . ",\n\nThank you for registering with Star Dream Cakes. Please click the link below to verify your account:\n\nhttp://www.ivanbrazza.biz/sdc/verify-email/?email=" . $_POST['email'] . "&code=" . $email_verification . "\n\n\nThank You,\nStar Dream Cakes";
-  $headers  = "From:" . $from;
+  $body     = "Hi " . $_POST['first_name'] . ",\n\nThank you for registering with Star Dream Cakes. Please click the link below to verify your account:\n\nhttp://www.ivanbrazza.biz/sdc/verify-email/?email=" . $_POST['email'] . "&code=" . $email_verification . "\n\n\nThank You,\nStar Dream Cakes";
+  $headers  = array(
+                'From'    => $from,
+                'To'      => $to,
+                'Subject' => $subject
+              );
   
-  mail($to, $subject, $message, $headers);
+  $smtp = Mail::factory('smtp', array(
+    'host'      => 'oxmail.registrar-servers.com',
+    'port'      => '25',
+    'auth'      => true,
+    'username'  => 'admin@ivanbrazza.biz',
+    'password'  => 'inspiron1520',
+    'timeout'   => '30'
+  ));
+
+  $mail = $smtp->send($to, $headers, $body);
+
+  if (PEAR::isError($mail)) {
+    die("Failed to send email: " . $mail->getMessage());
+  }
 ?>
