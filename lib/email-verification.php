32fab7062759933667986786e13baf90b46c4a15
Ivan Brazza <dudeman1996@gmail.com>
2013-10-20 15:21:09 +0100

    Fix email verification link

--- lib/email-verification.php
+++ lib/email-verification.php
@@ -8,7 +8,7 @@
   $from     = "Star Dream Cakes <admin@ivanbrazza.biz>";
   $to       = $_POST['email'];
   $subject  = "Register Your Email";
-  $body     = "Hi " . $_POST['first_name'] . ",\n\nThank you for registering with Star Dream Cakes. Please click the link below to verify your account:\n\nhttp://www.ivanbrazza.biz/sdc/verify-email/?email=" . $_POST['email'] . "&code=" . $email_verification . "\n\n\nThank You,\nStar Dream Cakes";
+  $body     = "Hi " . $_POST['first_name'] . ",\n\nThank you for registering with Star Dream Cakes. Please click the link below to verify your account:\n\nhttp://www.ivanbrazza.biz/verify-email/?email=" . $_POST['email'] . "&code=" . $email_verification . "\n\n\nThank You,\nStar Dream Cakes";
   $headers  = array(
                 'From'    => $from,
                 'To'      => $to,
