fe7d87cb1694f950d13794dc5d5418f0f7c2f601
Ivan Brazza <dudeman1996@gmail.com>
2013-10-22 13:13:26 +0100

    Add home header

--- home/index.php
+++ home/index.php
@@ -4,6 +4,7 @@
   $page = "home";
 ?>
 <?php include("../lib/header.php"); ?>
+  <h1>Home</h1>
   <p>Fran Bacelar, the creative talent behind Star Dream Cakes, has been making celebration cakes for over 20 years. Her creations for customers and friends have gained her respect locally.</p>
   <p>Fran holds the City and Guilds certificate in Sugarcraft and design and Wilton Method of Cake Decorating.</p>
   <p>As you may have seen in the photo gallery, celebration cakes are her speciality. She makes cakes for birthdays, anniversaries, christenings, corporate events etc. Within our ranges we have products which are gluten-free, dairy-free and products suitable for both vegetarians and vegans. If there is any particular design you would like on your cake, as long as it is within reason, she can make the ideal cake for you.</p>
