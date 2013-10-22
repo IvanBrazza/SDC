b9f34cfdbe696e47b93b2f58fc1b952c1c1e93e5
Ivan Brazza <dudeman1996@gmail.com>
2013-10-22 13:04:51 +0100

    Add homepage text

--- home/index.php
+++ home/index.php
@@ -2,7 +2,11 @@
   require("../lib/common.php");
   $title = "Home";
   $page = "home";
-
-  include("../lib/header.php");
-  include("../lib/footer.php");
 ?>
+<?php include("../lib/header.php"); ?>
+  <p>Fran Bacelar, the creative talent behind Star Dream Cakes, has been making celebration cakes for over 20 years. Her creations for customers and friends have gained her respect locally.</p>
+  <p>Fran holds the City and Guilds certificate in Sugarcraft and design and Wilton Method of Cake Decorating.</p>
+  <p>As you may have seen in the photo gallery, celebration cakes are her speciality. She makes cakes for birthdays, anniversaries, christenings, corporate events etc. Within our ranges we have products which are gluten-free, dairy-free and products suitable for both vegetarians and vegans. If there is any particular design you would like on your cake, as long as it is within reason, she can make the ideal cake for you.</p>
+  <p>Also, if you live in Haringey, Enfield or Barnet, you may be eligible for home delivery with an additional charge.</p>
+  <p>Your cake is made for you and you alone therefore we will do our best to meet most of your requirements.</p>
+<?php include("../lib/footer.php"); ?>
