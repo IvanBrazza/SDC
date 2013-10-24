181914a89004385453c3a60b667cc0d29ab6ffbd
Ivan Brazza <dudeman1996@gmail.com>
2013-10-24 22:43:13 +0100

    Switch origin and destination

--- lib/distance.php
+++ lib/distance.php
@@ -2,8 +2,8 @@
   function calculateDistance($address, $postcode) {
     $address = str_replace(" ", "+", $address);
     $postcode = str_replace(" ", "+", $postcode);
-    $origins = $address . "," . $postcode;
-    $url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" . $origins . "&destinations=95+Hoe+Lane,EN35SW&sensor=false&units=imperial";
+    $destination = $address . "," . $postcode;
+    $url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=95+Hoe+Lane,EN35SW&destinations=" . $destination . "&sensor=false&units=imperial";
     $json = file_get_contents($url);
     $data = json_decode($json, TRUE);
     $meters = $data['rows'][0]['elements'][0]['distance']['value'];
