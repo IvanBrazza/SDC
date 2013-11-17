84ae62511b674d1a152b4bddd36bfec7ebde934e
Ivan Brazza <dudeman1996@gmail.com>
2013-11-17 18:06:37 +0000

    Browser check script is local to avoid http/https issues

--- lib/header.php
+++ lib/header.php
@@ -19,7 +19,7 @@
       try {if ($buoop.ol) $buoop.ol();}catch (e) {} 
       var e = document.createElement("script"); 
       e.setAttribute("type", "text/javascript"); 
-      e.setAttribute("src", "http://browser-update.org/update.js"); 
+      e.setAttribute("src", "../js/browser.js"); 
       document.body.appendChild(e); 
     } 
   </script> 
