59341b6b62e05297a3644caf7eb2c0a6ad81e6b4
Ivan Brazza <dudeman1996@gmail.com>
2014-01-14 04:58:13 -0500

    Konami! :3

--- js/main.js
+++ js/main.js
@@ -2,8 +2,25 @@ var $buoop = {
   vs: {i:9,f:15,o:10.6,s:4,n:10},
   reminder: 0,
   newwindow: true
-} 
+  },
+  konami = '38,38,40,40,37,39,37,39,66,65',
+  keys   = [];
 $(document).ready(function() {
+  $(document).keydown(function(e) {
+    keys.push(e.keyCode);
+    if (keys.toString().indexOf(konami) >=0) {
+      $(".logo>img").animate({width: 500, height: 200}, function() {
+        $(this).animate({width:80, height: 50});
+      });
+      setInterval(function() {
+        $(".container").children().effect("shake", 400, function() {
+          $(".container").children().effect("bounce", 400);
+        });
+      }, 800);
+      keys = [];
+    }
+  });
+
   $('table#orders-js>tbody>tr').click(function() {
     window.location.href = $(this).find("a").attr("href");
   }).hover(function() {
