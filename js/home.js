b2c55fe23a2575720ef26c3a333f10cd8c03c753
Ivan Brazza <dudeman1996@gmail.com>
2014-05-07 10:32:45 -0400

    Update carousel logic
    -> Actually use wmOptions
    -> Reload images when the window width changes (i.e. a mobile device changes from portrati to landscape

--- js/home.js
+++ js/home.js
@@ -1,87 +1,111 @@
+var wmOptions = {
+      autoResize: true,
+      itemWidth: 200,
+      align: "center",
+      direction: "right",
+      flexibleWidth: true,
+      container: "",
+      offset: 0,
+      verticalOffset: 0,
+      fillEmptySapce: false
+    },
+    curSlide  = 1,
+    curWidth  = window.innerWidth,
+    d         = 0,
+    $slide1,
+    $slide2,
+    $slide3;
 $(document).ready(function() {
-  var wmOptions = {
-        autoResize: true,
-        itemWidth: 200,
-        align: "center",
-        direction: "right",
-        flexibleWidth: true,
-        offset: 0,
-        verticalOffset: 0,
-        fillEmptySapce: false
-      },
-      d = 0;
+  howManyImages();
+  $slide1.data("refresh", "true");
+  $slide2.data("refresh", "true");
+  $slide3.data("refresh", "true");
+
+  $("#myCarousel").on('slid.bs.carousel', function() {
+    var curSlide = $(".carousel-inner").find(".active").index() + 1;
+    switch (curSlide) {
+      case 1:
+        if ($slide1.data("refresh") == "true") {
+          $slide1.trigger("refreshWookmark").each(function() {
+            $(this).delay(d).fadeIn();
+            d += 100;
+          });
+          d = 0;
+          $slide1.data("refresh", "false");
+        }
+        break;
+      case 2:
+        if ($slide2.data("refresh") == "true") {
+          $slide2.trigger("refreshWookmark").each(function() {
+            $(this).delay(d).fadeIn();
+            d += 100;
+          });
+          d = 0;
+          $slide2.data("refresh", "false");
+        }
+        break;
+      case 3:
+        if ($slide3.data("refresh") == "true") {
+          $slide3.trigger("refreshWookmark").each(function() {
+            $(this).delay(d).fadeIn();
+            d += 100;
+          });
+          d = 0;
+          $slide3.data("refresh", "false");
+        }
+        break;
+    }
+  });
+
+  $(window).resize(function() {
+    if (curWidth != window.innerWidth) {
+      $slide1.hide();
+      $slide2.hide();
+      $slide3.hide();
+      howManyImages();
+      $slide1.data("refresh", "true");
+      $slide2.data("refresh", "true");
+      $slide3.data("refresh", "true");
+      curWidth = window.innerWidth;
+      $("#myCarousel").trigger('slid.bs.carousel');
+    }
+  });
+});
+
+function howManyImages() {
   if (window.innerWidth < 768) {
-    var $slide1 = $("#slide1-wm-tiles li:lt(8)"),
-        $slide2 = $("#slide2-wm-tiles li:lt(8)"),
-        $slide3 = $("#slide3-wm-tiles li:lt(8)");
+    $slide1 = $("#slide1-wm-tiles li:lt(8)"),
+    $slide2 = $("#slide2-wm-tiles li:lt(8)"),
+    $slide3 = $("#slide3-wm-tiles li:lt(8)");
   } else if (window.innerWidth < 992) {
-    var $slide1 = $("#slide1-wm-tiles li:lt(10)"),
-        $slide2 = $("#slide2-wm-tiles li:lt(11)"),
-        $slide3 = $("#slide3-wm-tiles li:lt(11)");
+    $slide1 = $("#slide1-wm-tiles li:lt(10)"),
+    $slide2 = $("#slide2-wm-tiles li:lt(11)"),
+    $slide3 = $("#slide3-wm-tiles li:lt(11)");
   } else {
-    var $slide1 = $("#slide1-wm-tiles li"),
-        $slide2 = $("#slide2-wm-tiles li"),
-        $slide3 = $("#slide3-wm-tiles li");
+    $slide1 = $("#slide1-wm-tiles li"),
+    $slide2 = $("#slide2-wm-tiles li"),
+    $slide3 = $("#slide3-wm-tiles li");
   }
+  $slide1.find("img.lazy").unveil();
+  $slide2.find("img.lazy").unveil();
+  $slide3.find("img.lazy").unveil();
+
   $slide1.imagesLoaded(function() {
-    $slide1.wookmark({
-        autoResize: true,
-        itemWidth: 200,
-        align: "center",
-        direction: "right",
-        flexibleWidth: true,
-        container: $("#slide1-wm"),
-        offset: 0,
-        verticalOffset: 0,
-        fillEmptySapce: false
-      }).hide().each(function() {
+    wmOptions.container = $("#slide1-wm");
+    $slide1.wookmark(wmOptions).hide().each(function() {
       $(this).delay(d).fadeIn();
       d += 100;
     });
     d = 0;
-  }).find("img.lazy").unveil();
-  $slide2.imagesLoaded(function() {
-    $slide2.wookmark({
-        autoResize: true,
-        itemWidth: 200,
-        align: "center",
-        direction: "right",
-        flexibleWidth: true,
-        container: $("#slide2-wm"),
-        offset: 0,
-        verticalOffset: 0,
-        fillEmptySapce: false
-      }).hide();
-  }).find("img.lazy").unveil();
-  $slide3.imagesLoaded(function() {
-    $slide3.wookmark({
-        autoResize: true,
-        itemWidth: 200,
-        align: "center",
-        direction: "right",
-        flexibleWidth: true,
-        container: $("#slide3-wm"),
-        offset: 0,
-        verticalOffset: 0,
-        fillEmptySapce: false
-      }).hide();
-  }).find("img.lazy").unveil();
-  var slide = 1;
-  $("#myCarousel").on('slid.bs.carousel', function() {
-    if (slide < 3) {
-      slide++;
-      if (slide == 2) {
-        $slide2.trigger("refreshWookmark").each(function() {
-          $(this).delay(d).fadeIn();
-          d += 100;
-        });
-        d = 0;
-      } else if (slide == 3) {
-        $slide3.trigger("refreshWookmark").each(function() {
-          $(this).delay(d).fadeIn();
-          d += 100;
-        });
-      }
-    }
   });
-});
+
+  $slide2.imagesLoaded(function() {
+    wmOptions.container = $("#slide2-wm");
+    $slide2.wookmark(wmOptions).hide();
+  });
+
+  $slide3.imagesLoaded(function() {
+    wmOptions.container = $("#slide3-wm");
+    $slide3.wookmark(wmOptions).hide();
+  });
+}
