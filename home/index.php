5bf3c7a17eb2ae90ebca6a104be7fff55f607f16
Ivan Brazza <dudeman1996@gmail.com>
2014-05-14 12:09:38 -0400

    Reduce carousel slide time

--- home/index.php
+++ home/index.php
@@ -68,7 +68,7 @@
       </div>
     </div>
   </nav>
-  <div id="myCarousel" class="carousel slide" data-ride="carousel" data-wrap="false" data-interval="10000">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel" data-wrap="false" data-interval="8000">
     <ol class="carousel-indicators">
       <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
       <li data-target="#myCarousel" data-slide-to="1"></li>
