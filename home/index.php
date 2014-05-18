67b6da6f7aaef25d64c45c376318c951600203cf
Ivan Brazza <dudeman1996@gmail.com>
2014-05-18 08:48:49 -0400

    Don't stop the carousel ever

--- home/index.php
+++ home/index.php
@@ -68,7 +68,7 @@
       </div>
     </div>
   </nav>
-  <div id="myCarousel" class="carousel slide" data-ride="carousel" data-wrap="false" data-interval="8000">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel" data-wrap="false" data-interval="8000" data-pause="false">
     <ol class="carousel-indicators">
       <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
       <li data-target="#myCarousel" data-slide-to="1"></li>
